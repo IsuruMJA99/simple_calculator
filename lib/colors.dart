cf22cb606053b3ae19f4fcada6306c8155e7cbef
IsuruMJA99 <mjaisurumadusanka@gmail.com>
2023-11-27 03:36:50 +0530

    starting point, generated plugins

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const operatorColor = Color(0xff272727);
+const buttonColor = Color(0xff191919);
+const orangeColor = Color(0xffD9802E);
+
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:calculator/colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: CalculatorApp(),
+  ));
+}
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(children: [
+        Expanded(child: Container(color: Colors.red)),
+        //button area
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(22),
+                    primary: buttonColor,
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "1",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ]),
+    );
+  }
+}
